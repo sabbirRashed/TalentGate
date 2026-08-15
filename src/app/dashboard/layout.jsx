ced9c3e5bb3ad8f6ce6c67746799be1c3c0d8f5e
sabbirRashed <srsabbirrahman12@gmail.com>
2026-08-15 23:13:55 +0600

    add sidebar for dashboard

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,15 @@
+import { DashboardSidebar } from '@/components/Dashboard/DashboardSidebar';
+import React from 'react';
+
+const DashboardLayout = ({children}) => {
+    return (
+        <div className='flex min-h-screen border-t border-default'>
+            <div>
+                <DashboardSidebar/>
+            </div>
+            <div className='flex-1'>{children}</div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
---
+++ src/app/dashboard/recruiter/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecruiterDashboard = () => {
+    return (
+        <div>
+            Hello, I'm Recruiter.
+        </div>
+    );
+};
+
+export default RecruiterDashboard;
