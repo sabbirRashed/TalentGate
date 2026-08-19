216663e8ce0bc3a6688c907d2d36f2fcdaa351dd
sabbirRashed <srsabbirrahman12@gmail.com>
2026-08-19 15:14:03 +0600

    feat: create logedInRecruiterCompany fetch function and fetch logedInRecruiter company

--- src/lib/api/company.js
+++ src/lib/api/company.js
@@ -1,6 +1,12 @@
 import { serverFetch } from "../core/server"
+import { getUserSession } from "../core/session";
 
 
 export const getRecruiterCompany = async(recruiterId)=>{
     return serverFetch(`/api/my/company?recruiterId=${recruiterId}`);
+}
+
+export const getLogedInRecruiterCompany = async()=>{
+    const user = await getUserSession();
+    return getRecruiterCompany(user?.id)
 }
